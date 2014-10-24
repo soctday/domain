48c030d3d8984c65fd7d46b4fc1fbf4b6dc28057
It <it@bluespiriter.com>
2014-10-24 17:53:57 +0800

    asp.net mvc 实现二级域名
    搭配泛解析使用
    常见问题：
    http://www.guzongxiang.com/Article/435.aspx

---
+++ Domain/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Domain.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var subdomain = RouteData.Values["subdomain"].ToString();
+            ViewBag.Domain = subdomain;
+            return View();
+        }
+
+    }
+}
